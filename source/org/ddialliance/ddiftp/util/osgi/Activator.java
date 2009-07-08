e9ecb6b2ebd50d05d3573935b9b5ecbcd58e16f8
Jannik Vestergaard Jensen <jvj@dda.dk>
2009-07-08 08:54:52 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/util@497 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ source/org/ddialliance/ddiftp/util/osgi/Activator.java
@@ -0,0 +1,24 @@
+package org.ddialliance.ddiftp.util.osgi;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		// System.out.println("Bundle util started");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		// System.out.println("Bundle util stopped");
+	}
+
+}
