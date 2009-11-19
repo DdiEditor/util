e070c9ab6bcaa5bd60021a4c593d17ca7bf7fcd4
Dan Kristiansen <dak@dda.dk>
2009-11-19 14:29:58 +0000

    Minor update
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/util@737 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- source/org/ddialliance/ddiftp/util/xml/XmlBeansUtil.java
+++ source/org/ddialliance/ddiftp/util/xml/XmlBeansUtil.java
@@ -30,8 +30,7 @@ import org.xml.sax.helpers.DefaultHandler;
  * A collection of XmlBean utilities to access the DDI
  */
 public class XmlBeansUtil {
-	private static Log logSystem = LogFactory.getLog(LogType.SYSTEM,
-			XmlBeansUtil.class);
+	private static Log logSystem = LogFactory.getLog(LogType.SYSTEM, XmlBeansUtil.class);
 
 	/**
 	 * Open a DDI document
@@ -43,8 +42,7 @@ public class XmlBeansUtil {
 	 * @return xml object of type document
 	 * @throws DDIFtpException
 	 */
-	public static <T extends XmlObject> T openDDIDoc(File file)
-			throws DDIFtpException {
+	public static <T extends XmlObject> T openDDIDoc(File file) throws DDIFtpException {
 
 		// get first element
 		String className = null;
@@ -70,8 +68,7 @@ public class XmlBeansUtil {
 			// get the first element name
 			className = e.getMessage();
 		} catch (IOException e) {
-			throw new DDIFtpException("file.not.found", file.getAbsoluteFile(),
-					e);
+			throw new DDIFtpException("file.not.found", file.getAbsoluteFile(), e);
 		}
 		return (T) openDDI(file, null, className);
 	}
@@ -91,15 +88,13 @@ public class XmlBeansUtil {
 	 * @return xml object of type document
 	 * @throws DDIFtpException
 	 */
-	public static <T extends XmlObject> T openDDI(Object ddi,
-			String ddiVersion, String className) throws DDIFtpException {
+	public static <T extends XmlObject> T openDDI(Object ddi, String ddiVersion, String className)
+			throws DDIFtpException {
 		// check classname
 		if (className.indexOf(".") < 0) {
-			throw new DDIFtpException("xmlbeanutil.open.classname",
-					new Object[] { className });
+			throw new DDIFtpException("xmlbeanutil.open.classname", new Object[] { className });
 		}
-		className = Config.get(Config.DDI3_XMLBEANS_BASEPACKAGE) + className
-				+ "Document";
+		className = Config.get(Config.DDI3_XMLBEANS_BASEPACKAGE) + className + "Document";
 		if (logSystem.isDebugEnabled()) {
 			logSystem.debug("Classname: " + className);
 		}
@@ -109,8 +104,7 @@ public class XmlBeansUtil {
 		options.setLoadLineNumbers();
 		options.setLoadLineNumbers(XmlOptions.LOAD_LINE_NUMBERS_END_ELEMENT);
 		try {
-			obj = ReflectionUtil.invokeStaticMethod(className + "$Factory",
-					"parse", ddi, options);
+			obj = ReflectionUtil.invokeStaticMethod(className + "$Factory", "parse", ddi, options);
 		} catch (Exception e) {
 			String errorResoure = null;
 			if (ddi instanceof File) {
@@ -120,16 +114,14 @@ public class XmlBeansUtil {
 			} else {
 				errorResoure = "n/a";
 			}
-			DDIFtpException wrapedException = new DDIFtpException(
-					"xmlbeanutil.open.error", errorResoure);
+			DDIFtpException wrapedException = new DDIFtpException("xmlbeanutil.open.error", errorResoure);
 			wrapedException.setRealThrowable(e);
 			throw wrapedException;
 		}
 		return (T) obj;
 	}
 
-	public static void saveDDIDoc(XmlObject xmlObject, File file)
-			throws Exception {
+	public static void saveDDIDoc(XmlObject xmlObject, File file) throws Exception {
 		if (!file.exists()) {
 			file.createNewFile();
 		}
@@ -153,8 +145,7 @@ public class XmlBeansUtil {
 	 *            of type XmlObject
 	 * @return xml object
 	 */
-	public static <T extends XmlObject> T nodeToXmlObject(
-			Class<? extends XmlObject> c, Node node) {
+	public static <T extends XmlObject> T nodeToXmlObject(Class<? extends XmlObject> c, Node node) {
 		// XmlObject.Factory.parse(node) does not return proper mapping of
 		// child nodes. Work around:
 
@@ -174,8 +165,7 @@ public class XmlBeansUtil {
 	 *            to set
 	 * @return altered xml object
 	 */
-	public static XmlObject setTextOnMixedElement(XmlObject xmlObject,
-			String text) {
+	public static XmlObject setTextOnMixedElement(XmlObject xmlObject, String text) {
 		XmlCursor xmlCursor = xmlObject.newCursor();
 
 		// insert new text
@@ -193,8 +183,7 @@ public class XmlBeansUtil {
 	public static void debugXmlCursor(XmlObject xmlObject) {
 		XmlCursor xmlCursor = xmlObject.newCursor();
 		while (!xmlCursor.toNextToken().isNone()) {
-			System.out.println("debug "
-					+ xmlCursor.currentTokenType().toString());
+			System.out.println("debug " + xmlCursor.currentTokenType().toString());
 			System.out.println("debug " + xmlCursor.toString());
 		}
 		xmlCursor.dispose();
@@ -232,13 +221,10 @@ public class XmlBeansUtil {
 	 *            throwable reference to checking method
 	 * @throws DDIFtpException
 	 */
-	public static void instanceOfXmlBeanDocument(XmlObject xmlObject,
-			Throwable throwable) throws DDIFtpException {
+	public static void instanceOfXmlBeanDocument(XmlObject xmlObject, Throwable throwable) throws DDIFtpException {
 		if (xmlObject.getClass().getName().indexOf("Document") <= -1) {
-			DDIFtpException exception = new DDIFtpException("XmlBean: "
-					+ xmlObject.getClass().getName() + " must be of type: "
-					+ xmlObject.getDomNode().getLocalName() + "Document",
-					throwable);
+			DDIFtpException exception = new DDIFtpException("XmlBean: " + xmlObject.getClass().getName()
+					+ " must be of type: " + xmlObject.getDomNode().getLocalName() + "Document", throwable);
 			throw exception;
 		}
 	}
@@ -253,8 +239,8 @@ public class XmlBeansUtil {
 	 * @return xml object type
 	 * @throws Exception
 	 */
-	public static XmlObject getXmlObjectTypeFromXmlDocument(
-			XmlObject xmlObject, Throwable throwable) throws DDIFtpException {
+	public static XmlObject getXmlObjectTypeFromXmlDocument(XmlObject xmlObject, Throwable throwable)
+			throws DDIFtpException {
 		instanceOfXmlBeanDocument(xmlObject, throwable);
 
 		// retrieve local name of node
@@ -264,8 +250,7 @@ public class XmlBeansUtil {
 
 		// retrieve xml object type
 		try {
-			return (XmlObject) ReflectionUtil.invokeMethod(xmlObject, "get"
-					+ localName, false, null);
+			return (XmlObject) ReflectionUtil.invokeMethod(xmlObject, "get" + localName, false, null);
 		} catch (Exception e) {
 			throw new DDIFtpException("Error on xmlObjet get", e);
 		}
@@ -303,14 +288,9 @@ public class XmlBeansUtil {
 			boolean translateable) throws DDIFtpException {
 		try {
 			ReflectionUtil.invokeMethod(xmlObject, "setTranslated", false,
-					true);
-//			ReflectionUtil.invokeMethod(xmlObject, "setTranslateable", false,
-//					translateable);
-			XmlBoolean xmlBoolean = org.apache.xmlbeans.XmlBoolean.Factory.newInstance();
-			xmlBoolean.setBooleanValue(translateable);
-//			ReflectionUtil.invokeMethod(xmlObject, "xsetTranslateable", false,
-//					xmlBoolean);
-			//xsetTranslatable(org.apache.xmlbeans.XmlBoolean translatable)
+					translated);
+			ReflectionUtil.invokeMethod(xmlObject, "setTranslatable", false,
+					translateable);			
 			ReflectionUtil.invokeMethod(xmlObject, "setLang", false, lang);
 		} catch (Exception e) {
 			throw new DDIFtpException("Set translations args error on: {0}",
@@ -340,8 +320,8 @@ public class XmlBeansUtil {
 		xmlOptions.setSavePrettyPrint();
 		XmlObject xmlObject = null;
 		for (Object item : items) {
-			if (!Boolean.parseBoolean(XmlBeansUtil.getXmlAttributeValue(
-					((XmlObject) item).xmlText(xmlOptions), "translated=\""))) {
+			if (!Boolean.parseBoolean(XmlBeansUtil.getXmlAttributeValue(((XmlObject) item).xmlText(xmlOptions),
+					"translated=\""))) {
 				xmlObject = (XmlObject) item;
 				// break;
 			}
