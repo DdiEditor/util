208af550336cc734ec87e5e26826c72f730c36ea
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-06-18 12:41:22 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/util@1257 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ source/org/ddialliance/ddiftp/util/DdiEditorConfig.java
@@ -0,0 +1,18 @@
+package org.ddialliance.ddiftp.util;
+
+import org.ddialliance.ddiftp.util.Config;
+import org.ddialliance.ddiftp.util.log.LogFactory;
+import org.ddialliance.ddiftp.util.log.LogType;
+
+public class DdiEditorConfig extends Config {
+	// dbxml
+	public static final String DBXML_ENVIROMENT_HOME = "dbxml.enviroment.home";
+	public static final String DBXML_IMPORT_VALIDATE = "dbxml.import.validate";
+	public static final String DBXML_DDI_INDEX = "dbxml.index.ddi";
+	public static final String DDI_ELEMENTS_RELATIONSHIP_LIST = "ddi.elements.relationshiplist";
+	
+	public static void init() {
+		LogFactory.getLog(LogType.SYSTEM, DdiEditorConfig.class).info(
+				"DDI editor init config done ;- )");
+	}
+}
