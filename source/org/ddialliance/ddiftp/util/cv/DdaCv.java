e6a67519937a80c4c71b2f459fa4827c39585cde
Jannik Vestergaard Jensen <jvj@dda.dk>
2012-11-24 22:52:25 +0000

    Added new entries
    git-svn-id: svn://192.168.99.101/dda/branches/ddieditor/corranhorn/ddiftp/util@4191 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- source/org/ddialliance/ddiftp/util/cv/DdaCv.java
+++ source/org/ddialliance/ddiftp/util/cv/DdaCv.java
@@ -1,9 +1,19 @@
 package org.ddialliance.ddiftp.util.cv;
 
 public enum DdaCv {
-	STUDY_STATE("studystate", "1.0.0"), ACCESS_RESTRICTION(
-			"accessrestrictions", "1.0.0"), ACCESS_CONDITION(
-			"accessconditions", "1.0.0");
+	STUDY_STATE("studystate", "1.0.0"),
+
+	ACCESS_RESTRICTION("accessrestrictions", "1.0.0"),
+
+	ACCESS_CONDITION("accessconditions", "1.0.0"),
+
+	KIND_OF_DATA("kindofdata", "1.0.0"),
+
+	DATA_COLLECTION_METHODOLOGY("datacollectionmethodology", "1.0.0"),
+	
+	DATA_COLLECTION_MODE("datacollectionmode", "1.0.0"),
+	
+	SAMPLING_PROCEDURE("samplingprocedure", "1.0.0");
 
 	private String name;
 	private String version;
