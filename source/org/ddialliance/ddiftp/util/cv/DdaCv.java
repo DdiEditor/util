ea6f5d37aa79587d2a5842d72f331633c2c701c4
Jannik Vestergaard Jensen <jvj@dda.dk>
2012-11-26 18:51:10 +0000

    Various adds collection event rest plus methodology rest
    git-svn-id: svn://192.168.99.101/dda/branches/ddieditor/corranhorn/ddiftp/util@4201 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- source/org/ddialliance/ddiftp/util/cv/DdaCv.java
+++ source/org/ddialliance/ddiftp/util/cv/DdaCv.java
@@ -1,19 +1,35 @@
 package org.ddialliance.ddiftp.util.cv;
 
 public enum DdaCv {
+	//
+	// study description
+	//
+	KIND_OF_DATA("kindofdata", "1.0.0"),
+
+	//
+	// data collection methodology
+	//
+	DATA_COLLECTION_METHODOLOGY("datacollectionmethodology", "1.0.0"),
+
+	DATA_COLLETION_TIME_METHOD("timemethod", "1.0.0"),
+
+	DATA_COLLECTION_SAMPLING_PROCEDURE("samplingprocedure", "1.0.0"),
+
+	//
+	// data collection event
+	//
+	DATA_COLLECTION_EVENT_MODE("datacollectionmode", "1.0.0"),
+
+	DATA_COLLECTION_EVENT_SITUATION("collectionsituation", "1.0.0"),
+
+	//
+	// archive
+	//
 	STUDY_STATE("studystate", "1.0.0"),
 
 	ACCESS_RESTRICTION("accessrestrictions", "1.0.0"),
 
-	ACCESS_CONDITION("accessconditions", "1.0.0"),
-
-	KIND_OF_DATA("kindofdata", "1.0.0"),
-
-	DATA_COLLECTION_METHODOLOGY("datacollectionmethodology", "1.0.0"),
-	
-	DATA_COLLECTION_MODE("datacollectionmode", "1.0.0"),
-	
-	SAMPLING_PROCEDURE("samplingprocedure", "1.0.0");
+	ACCESS_CONDITION("accessconditions", "1.0.0");
 
 	private String name;
 	private String version;
